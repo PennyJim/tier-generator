ffdb0dcd31be6e1c3a73256cbd112c8da939c69d
PennyJim <40438818+PennyJim@users.noreply.github.com>
2024-05-11 19:41:07 -0600

    Make recipe lookups

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -11,6 +11,55 @@ local function appendToArrayInTable(table, key, newValue)
 	table[key] = array;
 end
 
+---@type table<data.ItemID,data.RecipeID[]>
+local ItemRecipeLookup = {}
+---@type table<data.FluidID,data.RecipeID[]>
+local FluidRecipeLookup = {}
+---Parses `data.raw.recipe` items
+---@param recipeID data.RecipeID
+---@param recipePrototype data.RecipePrototype
+local function processRecipe(recipeID, recipePrototype)
+	---@type data.RecipePrototype|data.RecipeData
+	local recipeData = recipePrototype;
+
+	if recipeData.result == nil and recipeData.results == nil then
+		recipeData = recipeData.normal or recipeData.expensive
+		if recipeData == nil then
+			print(recipeID.." didn't result in anything?")
+			print(serpent.line(recipeData))
+			return;
+		end
+	end
+
+	if recipeData.result ~= nil then
+		appendToArrayInTable(ItemRecipeLookup, recipeData.result, recipeID)
+	elseif recipeData.results ~= nil then
+		for _, rawResult in pairs(recipeData.results) do
+			-- Get resultID
+				local result = rawResult[1] or rawResult.name;
+				if result == nil then
+					print("Couldn't find ingredientID:\n")
+					print(serpent.line(rawResult))
+					goto continue
+				end
+
+				if rawResult.type == "fluid" then
+					appendToArrayInTable(FluidRecipeLookup, result, recipeID)
+				else
+					appendToArrayInTable(ItemRecipeLookup, result, recipeID)
+				end
+			::continue::
+		end
+	else
+		print(recipeID.." didn't result in anything?")
+		print(serpent.line(recipeData))
+	end
+end
+
+for recipeID, rawRecipe in pairs(data.raw["recipe"]) do
+	processRecipe(recipeID, rawRecipe);
+end
+
 ---Determine the tier of the given prototype
 ---@param prototypeID string
 ---@param value data.ItemPrototype|data.FluidPrototype
