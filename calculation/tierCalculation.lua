08e4b07eb1d34d84652ebc1d6bca2fb575dd263d
PennyJim <40438818+PennyJim@users.noreply.github.com>
2024-05-19 16:21:08 -0600

    First pass of changes to runtime

--- calculation/tierCalculation.lua
+++ calculation/tierCalculation.lua
@@ -1,29 +1,55 @@
 local lookup = require("__tier-generator__.calculation.DataProcessing")
 local lib = require("__tier-generator__.library")
 
+---@alias fakePrototype {type:string}
+---@alias handledPrototypes fakePrototype|LuaRecipeCategoryPrototype|LuaTechnologyPrototype|LuaRecipePrototype|LuaFluidPrototype|LuaItemPrototype
+---@alias handledTypes "recipe-category"|"technology"|"recipe"|"fluid"|"item"
+
+---@enum invalidReason
+local invalidReason = {
+	calculating = -1,
+	no_machine = -2,
+	no_valid_machine = -3,
+	no_valid_technology = -4,
+	no_valid_furnace = -5,
+	no_valid_rocket = -6,
+	no_valid_recipe = -7,
+	error = -99
+}
+---@alias tier invalidReason|uint
+
+---@type table<uint,string[]>
 local tierArray = {};
+
+---@type table<handledTypes,table<string,number>>
 TierMaps = {
 	["recipe-category"] = {},
 	["technology"] = {},
 	["recipe"] = {},
 	["fluid"] = {},
+	["item"] = {}
 };
 for subtype in pairs(defines.prototypes["item"]) do
 	TierMaps[subtype] = {}
 end
+---@type table<handledTypes,table<string,boolean>>
+---@diagnostic disable-next-line: assign-type-mismatch
 local calculating = table.deepcopy(TierMaps)
 
 --#region Tier calculation
----@alias fakePrototype {type:string}
----@alias handledPrototypes fakePrototype|data.RecipeCategory|data.TechnologyPrototype|data.RecipePrototype|data.ItemPrototype|data.FluidPrototype
 ---@class TierSwitch
----@field [string] fun(prototypeID:string, value:handledPrototypes):number
----@overload fun(prototypeID:string,value:handledPrototypes):number
+---@field [string] fun(prototypeID:string, value:handledPrototypes):tier
+---@overload fun(prototypeID:string,value:handledPrototypes):tier
 local tierSwitch = setmetatable({}, {
+	---Base switching case of tierSwich
+	---@param self TierSwitch
+	---@param prototypeID string
+	---@param value handledPrototypes
+	---@return tier
 	__call = function(self, prototypeID, value)
 		local tier = TierMaps[value.type][prototypeID]
 		if tier ~= nil then return tier end
-		if calculating[value.type][prototypeID] then return -math.huge end
+		if calculating[value.type][prototypeID] then return invalidReason.calculating end
 
 		calculating[value.type][prototypeID] = true
 		local success = false
@@ -31,7 +57,7 @@ local tierSwitch = setmetatable({}, {
 		if not success then
 			-- _log({"error-calculating", prototypeID, value.type, serpent.dump(value)})
 			log("Error calculating the "..value.type.." of "..prototypeID..":\n"..tier)
-			tier = -math.huge
+			tier = invalidReason.error
 		end
 		calculating[value.type][prototypeID] = nil
 		if tier >= 0 then -- Discard negative values
@@ -44,14 +70,14 @@ local tierSwitch = setmetatable({}, {
 	end
 })
 ---Return the highest tier from the ingredients
----@param ingredients data.IngredientPrototype[]
+---@param ingredients Ingredient[]
 ---@return integer
 local function getIngredientsTier(ingredients)
 	local ingredientsTier = 0;
 	for _, ingredient in pairs(ingredients) do
-		local nextName = ingredient.name or ingredient[1]
-		local nextType = ingredient.type or lookup.ItemType[nextName]
-		local nextValue = data.raw[nextType][nextName]
+		local nextName = ingredient.name
+		local nextType = ingredient.type
+		local nextValue = lib.getItemOrFluid(nextName, nextType)
 		local nextTier = tierSwitch(nextName, nextValue)
 		-- Skip if machine takes an item being calculated
 		if nextTier < 0 then return nextTier end
@@ -61,38 +87,37 @@ local function getIngredientsTier(ingredients)
 end
 
 ---Determine the tier of the given technology
----@type fun(technologyID:data.TechnologyID,technology:data.TechnologyPrototype):number
+---@type fun(technologyID:data.TechnologyID,technology:LuaTechnologyPrototype):tier
 tierSwitch["technology"] = function (technologyID, technology)
-	local techData = lib.alwaysTechnologyData(technology)
-	local ingredientsTier = getIngredientsTier(techData.unit.ingredients)
+	local ingredients = technology.research_unit_ingredients
+	local ingredientsTier = getIngredientsTier(ingredients)
 
 	local prereqTier = 0;
-	for _, prerequisite in pairs(techData.prerequisites) do
-		local preValue = data.raw["technology"][prerequisite]
-		local preTier = tierSwitch(prerequisite, preValue)
+	for _, prerequisite in pairs(technology.prerequisites) do
+		local preTier = tierSwitch(prerequisite.name, prerequisite)
 		-- Skip if technology takes an item being calculated
-		if preTier < 0 then preTier = 0 end
+		if preTier < 0 then return preTier end
 		prereqTier = math.max(prereqTier, preTier)
 	end
 
 	if ingredientsTier == 0 and prereqTier == 0 then
-		error("I don't think a technology should every be t0")
+		log("I don't think a technology should ever be t0")
 	end
 	return math.max(ingredientsTier, prereqTier)
 end
 ---Determine the tier of the given recipe category
----@type fun(CategoryID:data.RecipeCategoryID,category:data.RecipeCategory):number
+---@type fun(CategoryID:data.RecipeCategoryID,category:LuaRecipeCategoryPrototype):tier
 tierSwitch["recipe-category"] = function (CategoryID, category)
 	local machines = lookup.CategoryItem[CategoryID]
 	if not machines then
 		lib.log("\tCategory "..CategoryID.." has no machines")
-		return -math.huge
+		return invalidReason.no_machine
 	end
 	local categoryTier = math.huge;
 	for _, item in pairs(machines) do
 		-- If it's craftable by hand, it's a base recipe.
 		if item == "hand" then return 0 end
-		local itemTier = tierSwitch(item, data.raw["item"][item])
+		local itemTier = tierSwitch(item, lib.getItem(item))
 		-- Don't consider the machine if it takes something being calculated.
 		-- It must mean that it uses something a tier too high, Right..?
 		if itemTier >= 0 then
@@ -101,7 +126,7 @@ tierSwitch["recipe-category"] = function (CategoryID, category)
 	end
 
 	if categoryTier == math.huge then
-		return -math.huge
+		return invalidReason.no_valid_machine
 	end
 
 	if settings.startup["tiergen-reduce-category"].value then
@@ -110,28 +135,27 @@ tierSwitch["recipe-category"] = function (CategoryID, category)
 	return categoryTier
 end
 ---Determine the tier of the given recipe
----@type fun(recipeID:data.RecipeID,recipe:data.RecipePrototype):number
+---@type fun(recipeID:data.RecipeID,recipe:LuaRecipePrototype):tier
 tierSwitch["recipe"] = function (recipeID, recipe)
-	local recipeData = lib.alwaysRecipeData(recipe)
-	if not recipeData.ingredients then
+	if #recipe.ingredients == 0 then
 		lib.log("\t"..recipeID.." didn't require anything? Means it's a t0?")
-		error(serpent.line(recipeData.ingredients))
+		return 0
 	end
 
 	-- Get recipe ingredients tier
-	local ingredientsTier = getIngredientsTier(recipeData.ingredients)
+	local ingredientsTier = getIngredientsTier(recipe.ingredients)
 	-- Exit early if child-tier isn't currently calculable
 	if ingredientsTier < 0 then return ingredientsTier end
 
 	-- Get category tier
-	local category = data.raw["recipe-category"][recipe.category or "crafting"]
+	local category = lib.getRecipeCategory(recipe.category)
 	local machineTier = tierSwitch(category.name, category)
 	-- Exit early if child-tier isn't currently calculable
 	if machineTier < 0 then return machineTier end
 
 	-- Get technology tier if it isn't enabled to start with
 	local technologyTier = 0
-	if not recipeData.enabled then
+	if not recipe.enabled then
 		technologyTier = math.huge
 		local technologies = lookup.RecipeTechnology[recipeID]
 		if not technologies then
@@ -140,7 +164,7 @@ tierSwitch["recipe"] = function (recipeID, recipe)
 		end
 
 		for _, technology in pairs(technologies) do
-			local nextValue = data.raw["technology"][technology]
+			local nextValue = lib.getTechnology(technology)
 			local nextTier = tierSwitch(technology, nextValue)
 			-- Assume currently calculating technology to be of higher tier
 			if nextTier >= 0 then
@@ -149,10 +173,14 @@ tierSwitch["recipe"] = function (recipeID, recipe)
 		end
 	end
 
+	if technologyTier == math.huge then
+		return invalidReason.no_valid_technology
+	end
+
 	return math.max(ingredientsTier, machineTier, technologyTier)
 end
 ---Determine the tier of burning an item
----@type fun(ItemID:data.ItemID,value:data.ItemPrototype):number
+---@type fun(ItemID:data.ItemID,value:LuaItemPrototype):tier
 tierSwitch["burning"] = function (ItemID, value)
 	local burningRecipes = lookup.Burning[ItemID]
 	local tier = math.huge
@@ -160,22 +188,28 @@ tierSwitch["burning"] = function (ItemID, value)
 	for _, fuelID in pairs(burningRecipes) do
 		local fuelTier = getIngredientsTier{{fuelID}}
 		if fuelTier < 0 then
-			return fuelTier
+			goto continue
 		end
-		local fuel = data.raw[lookup.ItemType[fuelID]][fuelID]
+		local fuel = lib.getItem(fuelID)
 		local categoryTier = tierSwitch("tiergen-fuel-"..fuel.fuel_category, {
 			type = "recipe-category",
 		})
 		if categoryTier < 0 then
-			return categoryTier
+			goto continue
 		end
 		local recipeTier = math.max(fuelTier, categoryTier)
 		tier = math.min(tier, recipeTier)
+	  	::continue::
 	end
+
+	if tier == math.huge then
+		return invalidReason.no_valid_furnace
+	end
+
 	return tier
 end
 ---Determine the tier of launching an item into space
----@type fun(ItemID:data.ItemID,value:data.ItemPrototype):number
+---@type fun(ItemID:data.ItemID,value:LuaItemPrototype):tier
 tierSwitch["rocket-launch"] = function (ItemID, value)
 	local rocketRecipes = lookup.Rocket[ItemID]
 	local tier = math.huge
@@ -183,21 +217,27 @@ tierSwitch["rocket-launch"] = function (ItemID, value)
 	for _, satelliteID in pairs(rocketRecipes) do
 		local satelliteTier = getIngredientsTier{{satelliteID}}
 		if satelliteTier < 0 then
-			return satelliteTier
+			goto continue
 		end
 		local categoryTier = tierSwitch("tiergen-rocket-launch", {
 			type = "recipe-category",
 		})
 		if categoryTier < 0 then
-			return categoryTier
+			goto continue
 		end
 		local recipeTier = math.max(satelliteTier, categoryTier)
 		tier = math.min(tier, recipeTier)
+	    ::continue::
 	end
+
+	if tier == math.huge then
+		return invalidReason.no_valid_rocket
+	end
+
 	return tier
 end
 ---Determine the tier of the given item or fluid
----@type fun(ItemID:data.ItemID|data.FluidID,value:data.ItemPrototype|data.FluidPrototype):number
+---@type fun(ItemID:data.ItemID|data.FluidID,value:LuaItemPrototype|LuaFluidPrototype):tier
 tierSwitch["fluid"] = function (ItemID, value)
 	local recipes
 	if value.type == "fluid" then
@@ -213,14 +253,14 @@ tierSwitch["fluid"] = function (ItemID, value)
 	local recipeTier = math.huge
 	for _, recipe in pairs(recipes) do
 		-- if the recipeID starts with "tiergen-" then it's a fake recipe
-		-- this mod *will not* make recipes (not for smuggling this data out)
+		-- this mod *will not* make recipes
 		local _, realstart = recipe:find("^tiergen[-]")
 		local tempTier = math.huge
 		if realstart then
 			local fakeRecipeID = recipe:sub(realstart+1)
 			tempTier = tierSwitch[fakeRecipeID](ItemID, value)
 		else
-			local recipePrototype = data.raw["recipe"][recipe]
+			local recipePrototype = lib.getRecipe(recipe)
 			tempTier = tierSwitch(recipe, recipePrototype)
 		end
 		-- Skip recipe if it's using something being calculated
@@ -234,7 +274,7 @@ tierSwitch["fluid"] = function (ItemID, value)
 	-- in the recipe processing, and skipped it if it there were *no* recipes.
 	-- This _must_ mean that there's something being calculated in the chain.
 	if recipeTier == math.huge then
-		return -1
+		return invalidReason.no_valid_recipe
 	end
 
 	return recipeTier + 1
@@ -248,15 +288,15 @@ end
 ---@param itemID string
 ---@return string?
 local function calculateTier(itemID)
-	local validItem, itemType = pcall(lib.resolveItemType, itemID)
-	if not validItem then
+	local isValid, itemPrototype = pcall(lib.getItem, itemID)
+	if not isValid then
 		log("\tWas given an invalid item: "..itemID)
 		return
 	end
 	local tier = -1
 	local rounds = 0
 	while tier < 0 and rounds < 5 do
-		tier = tierSwitch(itemID, data.raw[itemType][itemID])
+		tier = tierSwitch(itemID, itemPrototype)
 		rounds = rounds + 1
 	end
 	if rounds == 5 then
@@ -273,8 +313,12 @@ end
 ---Directly set the tier of a given itemID
 ---@param itemID string
 local function setTier(itemID)
-	local itemType = lib.resolveItemType(itemID)
-	TierMaps[itemType][itemID] = 0
+	local isValid, itemPrototype = pcall(lib.getItem, itemID)
+	if not isValid then
+		log("\tWas given an invalid item: "..itemID)
+		return
+	end
+	TierMaps[itemPrototype.type][itemID] = 0
 end
 
 return {
--- library.lua
+++ library.lua
@@ -32,18 +32,22 @@ end
 --#region Array Operations
 
 ---Appends to an array within a table
----@param table table
----@param key any
----@param newValue any
+---@generic Key : any
+---@generic Value : any
+---@param table table<Key,Value[]>
+---@param key Key
+---@param newValue Value
 function library.appendToArrayInTable(table, key, newValue)
 	local array = table[key] or {}
 	array[#array+1] = newValue;
 	table[key] = array;
 end
 ---Prepends to an array within a table
----@param table table
----@param key any
----@param newValue any
+---@generic Key : any
+---@generic Value : any
+---@param table table<Key,Value[]>
+---@param key Key
+---@param newValue Value
 function library.prependToArrayInTable(table, key, newValue)
 	local array = table[key] or {}
 	local shiftedValue = newValue
@@ -57,97 +61,157 @@ end
 --#endregion
 --#region data.raw standardization
 
----Always return plural results
----Absolutely no gurantees it works for everything
----@param table data.RecipePrototype|data.MinableProperties|any
----@return data.ProductPrototype[]
-function library.alwaysPluralResults(table)
-	---@type data.ProductPrototype[]
-	local results = table.results
-	if not results then
-		results = {{
-			type = "item",
-			name = table.result,
-		}}
-	end
-	if results[1] and results[1].name or results[1] and results[1][1] then
-		return results
-	else
----@diagnostic disable-next-line: return-type-mismatch
-		return false
-	end
-end
----Always return RecipeData
----@param table data.RecipePrototype
----@return data.RecipeData
-function library.alwaysRecipeData(table)
-	---@type data.RecipeData
----@diagnostic disable-next-line: assign-type-mismatch
-	local oldRecipeData = table.normal or table.expensive or {}
-	local recipeData = {
-		ingredients = table.ingredients or oldRecipeData.ingredients,
-		results = library.alwaysPluralResults(table) or library.alwaysPluralResults(oldRecipeData)
-	}
-	if table then
-		recipeData.enabled = table.enabled
-	elseif oldRecipeData then
-		recipeData.enabled = oldRecipeData.enabled
-	end
-	-- has to check if it's nil specifically, as it'll just always be true otherwise
-	if recipeData.enabled == nil then
-		recipeData.enabled = true;
-	end
+-- Theoretically unecessary now that its runtime
+-- ---Always return plural results
+-- ---Absolutely no gurantees it works for everything
+-- ---@param table data.RecipePrototype|data.MinableProperties|any
+-- ---@return data.ProductPrototype[]
+-- function library.alwaysPluralResults(table)
+-- 	---@type data.ProductPrototype[]
+-- 	local results = table.results
+-- 	if not results then
+-- 		results = {{
+-- 			type = "item",
+-- 			name = table.result,
+-- 		}}
+-- 	end
+-- 	if results[1] and results[1].name or results[1] and results[1][1] then
+-- 		return results
+-- 	else
+-- ---@diagnostic disable-next-line: return-type-mismatch
+-- 		return false
+-- 	end
+-- end
+-- ---Always return RecipeData
+-- ---@param table data.RecipePrototype
+-- ---@return data.RecipeData
+-- function library.alwaysRecipeData(table)
+-- 	---@type data.RecipeData
+-- ---@diagnostic disable-next-line: assign-type-mismatch
+-- 	local oldRecipeData = table.normal or table.expensive or {}
+-- 	local recipeData = {
+-- 		ingredients = table.ingredients or oldRecipeData.ingredients,
+-- 		results = library.alwaysPluralResults(table) or library.alwaysPluralResults(oldRecipeData)
+-- 	}
+-- 	if table then
+-- 		recipeData.enabled = table.enabled
+-- 	elseif oldRecipeData then
+-- 		recipeData.enabled = oldRecipeData.enabled
+-- 	end
+-- 	-- has to check if it's nil specifically, as it'll just always be true otherwise
+-- 	if recipeData.enabled == nil then
+-- 		recipeData.enabled = true;
+-- 	end
 
-	return recipeData
-end
----Always returns TechnologyData
----@param table data.TechnologyPrototype
----@return data.TechnologyData
-function library.alwaysTechnologyData(table)
-	---@type data.TechnologyData
----@diagnostic disable-next-line: assign-type-mismatch
-	local oldTechnologyData = table.normal or table.expensive or {}
-	local technologyData = {
-		unit = table.unit or oldTechnologyData.unit,
-		prerequisites = table.prerequisites or oldTechnologyData.prerequisites or {}
-	}
-	return technologyData
-end
+-- 	return recipeData
+-- end
+-- ---Always returns TechnologyData
+-- ---@param table data.TechnologyPrototype
+-- ---@return data.TechnologyData
+-- function library.alwaysTechnologyData(table)
+-- 	---@type data.TechnologyData
+-- ---@diagnostic disable-next-line: assign-type-mismatch
+-- 	local oldTechnologyData = table.normal or table.expensive or {}
+-- 	local technologyData = {
+-- 		unit = table.unit or oldTechnologyData.unit,
+-- 		prerequisites = table.prerequisites or oldTechnologyData.prerequisites or {}
+-- 	}
+-- 	return technologyData
+-- end
 --#endregion
 --#region Item Resolvers
 
+---A generic function to get a prototype
+---@generic P : any
+---@param name string
+---@param table table<string,P>
+---@return P
+local function getGeneric(name, table)
+	local prototype = table[name]
+	if not prototype then
+		error("Didn't find what we looked for: "..name, 2)
+	end
+	return prototype
+end
+
+---Returns the given item
+---@param name data.ItemID
+---@return LuaItemPrototype
+function library.getItem(name)
+	return getGeneric(name, game.item_prototypes)
+end
+---Returns the given fluid
+---@param name data.FluidID
+---@return LuaFluidPrototype
+function library.getFluid(name)
+	return getGeneric(name, game.fluid_prototypes)
+end
+---Returns the given item or fluid
+---@param name data.ItemID|data.FluidID
+---@param type "item"|"fluid"
+---@return LuaItemPrototype|LuaFluidPrototype
+function library.getItemOrFluid(name, type)
+	local table
+	if type == "item" then
+		table = game.item_prototypes
+	else
+		table = game.fluid_prototypes
+	end
+	return getGeneric(name, table)
+end
+---Returns the given recipe category
+---@param name data.RecipeCategoryID
+---@return LuaRecipeCategoryPrototype
+function library.getRecipeCategory(name)
+	return getGeneric(name, game.recipe_category_prototypes)
+end
+---Returns the given recipe category
+---@param name data.TechnologyID
+---@return LuaTechnologyPrototype
+function library.getTechnology(name)
+	return getGeneric(name, game.technology_prototypes)
+end
+---Returns the given recipe category
+---@param name data.RecipeID
+---@return LuaRecipePrototype
+function library.getRecipe(name)
+	return getGeneric(name, game.recipe_prototypes)
+end
+
+
+
 ---Gets the placable item that results in the entity
 ---@param EntityID data.EntityID
----@param entityPrototype data.EntityPrototype
----@return data.ItemID?
+---@param entityPrototype LuaEntityPrototype
+---@return data.ItemID[]?
 function library.getEntityItem(EntityID, entityPrototype)
-	if entityPrototype.placeable_by then
-		return entityPrototype.placeable_by.item
-	elseif entityPrototype.minable then
-		local items = library.alwaysPluralResults(entityPrototype.minable) or {}
+	local items = entityPrototype.items_to_place_this
+	if not items then
+		library.log("\t\t"..EntityID.." isn't placable. Ignoring...")
+		return nil
+	end
 
-		for _, item in pairs(items) do
-			if data.raw["item"][item.name].place_result == EntityID then
-				return item.name
-			end
-		end
-		
-		library.log("\t\t"..EntityID.."'s mined items aren't placable. Ignoring...")
-	else
-		library.log("\t\t"..EntityID.." Isn't placable _or_ mineable. Ignoring...")
-	end
-	return nil
+	return items
+	-- Unecessary?
+	-- for _, item in ipairs(items) do
+	-- 	local itemPrototype = library.getItemOrFluid(item.name, "item")
+
+	-- 	if itemPrototype.place_result and itemPrototype.place_result.name == EntityID then
+	-- 		return item.name
+	-- 	end
+	-- end
 end
----Resolves the type of an item, and errors if one is not found
----@param itemID string
----@return string
-function library.resolveItemType(itemID)
-	local itemType = lookup.ItemType[itemID]
-	if not itemType then
-		error("Could not find item type of given item: "..itemID)
-	end
-	return itemType
-end
+-- ---Resolves the type of an item, and errors if one is not found
+-- ---@param itemID string
+-- ---@return string
+-- ---@deprecated
+-- function library.resolveItemType(itemID)
+-- 	local itemType = lookup.ItemType[itemID]
+-- 	if not itemType then
+-- 		error("Could not find item type of given item: "..itemID)
+-- 	end
+-- 	return itemType
+-- end
 --#endregion
 
 return library
