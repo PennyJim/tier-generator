94e679260702ba66ed6db9b57bc5044f4663c351
PennyJim <40438818+PennyJim@users.noreply.github.com>
2024-07-04 19:29:31 -0600

    Migrate to 0.4

---
+++ migrations/0_4_0.lua
@@ -0,0 +1,11 @@
+-- Clean destroy old tiergen-menus
+for _, player in pairs(game.players) do
+	local menu = player.gui.screen["tiergen-menu"]
+	if menu then
+		menu.destroy()
+	end
+end
+
+-- Clear the old global values
+global.player = nil
+global.menu = nil
